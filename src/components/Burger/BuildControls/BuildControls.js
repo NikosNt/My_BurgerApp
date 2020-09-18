c5073af23b09ff4bbde4bd8082a4f543da315b54
Nikos-Nile <TreNikosD13@gmail.com>
2020-09-18 12:23:04 +0300

    HMU 1  commit

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './BuiltControls.css'
+
+const builtControls = (props)=> (
+
+    <div>
+
+    </div>
+);
+
+export default builtControls;
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,14 +6,27 @@ import BurgerIngredient from './BurgerIngerdient/BurgerIngredient'
 
 const burger = (props)=> {
 
-    return(
-      <div className={classes.Burger}>
-          <BurgerIngredient type="bread-top"/>
-          <BurgerIngredient type="cheese"/>
-          <BurgerIngredient type="meat"/>
-          <BurgerIngredient type="bread-bottom"/>
-      </div>  
-    );
+  let transformedIngredients = Object.keys(props.ingredients)//kanw to obj array gia na xrhsimopoiw to map
+      .map(igKey => {
+          return [...Array(props.ingredients[igKey])].map((_, i)=>{
+            return <BurgerIngredient key={igKey +i} type={igKey} />
+          });
+      })
+      .reduce((arr,el) => {//An exoume 0 ulika etsi kanoume ena array apo arrays se keno 
+        return arr.concat(el)
+      }, []);
+  
+if(transformedIngredients.length === 0){
+  transformedIngredients = <p>Please start adding ingredients !</p>
+}
+
+  return(
+    <div className={classes.Burger}>
+        <BurgerIngredient type="bread-top"/>
+          {transformedIngredients}
+        <BurgerIngredient type="bread-bottom"/>
+    </div>  
+  );
     
 };
 
