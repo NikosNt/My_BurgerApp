aa307ae1f6d988dd4b56e6dea3ef56a61e6b7210
Nikos-Nile <TreNikosD13@gmail.com>
2020-09-21 11:50:46 +0300

    first fase burger builder done

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,36 @@
-import React from 'react';
-import Aux from '../../../hoc/Auxiliary'
+import React, {Component} from 'react';
+import Aux from '../../../hoc/Auxiliary/Auxiliary'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
+class OrderSummary extends Component{
+//This could be a functional conponent ,doesnt have to be a class
 
-    const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
-    return <li key={igKey}>
-                <span style={{textTransform: 'capitalize'}}>{igKey}</span> : {props.ingredients[igKey]}
-           </li>
-    })
+    componentDidUpdate(){//componentWillUpdate
+        console.log('[OrderSummary] WillUpdate');
+    }
 
-    return(
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-    <p><strong>Total Price : {props.price.toFixed(2)}</strong></p>
-            <p> Continue to checkout ? </p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    )
-}
+    render(){
 
-export default orderSummary;
+        const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+        return <li key={igKey}>
+                    <span style={{textTransform: 'capitalize'}}>{igKey}</span> : {this.props.ingredients[igKey]}
+            </li>
+        })
+
+        return(
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price : {this.props.price.toFixed(2)}</strong></p>
+                <p> Continue to checkout ? </p>
+                <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>            
+        );
+    }
+} 
+
+export default OrderSummary;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,18 +1,20 @@
 import React from 'react';
+
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
-const toolbar = ( props) => (
+const toolbar = ( props ) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
+        <DrawerToggle clicked={props.drawerToggleClicked} />
         <div className={classes.Logo}>
-           <Logo /> 
+            <Logo />
         </div>
         <nav className={classes.DesktopOnly}>
-            <NavigationItems></NavigationItems>
+            <NavigationItems />
         </nav>
     </header>
 );
 
-export default toolbar
+export default toolbar;
